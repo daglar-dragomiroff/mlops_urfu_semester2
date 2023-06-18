25cb86bf46c0f057938a6bebe3f8f52d28f055d5
dragomirskiy <91115293+dragomirskiy@users.noreply.github.com>
2023-06-18 12:59:48 +0300

    Update create_model.py

--- final_project_with_jenkins/create_model.py
+++ final_project_with_jenkins/create_model.py
@@ -15,18 +15,5 @@ def create_model():
     with open('model.pkl', 'wb') as file:
         pickle.dump(model, file)
 
-def create_model():
-    dataset_1 = np.loadtxt('datasets/dataset_1.txt')
-
-    # Обучаем модель на dataset_1
-    model = LinearRegression()
-    X_train = dataset_1[:, 0].reshape(-1, 1)
-    y_train = dataset_1[:, 1]
-    model.fit(X_train, y_train)
-
-    # Сохранение модели
-    with open('model.pkl', 'wb') as file:
-        pickle.dump(model, file)
-
 if __name__=='__main__':
     create_model()
